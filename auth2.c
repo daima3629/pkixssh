7cc2bd290d0cb5c6606712bf631c08c532d37bd1
Roumen Petrov <pkixssh@roumenpetrov.info>
2018-05-22 20:06:58 +0300

    synchronize CVS ids

--- auth2.c
+++ auth2.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: auth2.c,v 1.146 2018/04/13 03:57:26 dtucker Exp $ */
+/* $OpenBSD: auth2.c,v 1.147 2018/05/11 03:22:55 dtucker Exp $ */
 /*
  * Copyright (c) 2000 Markus Friedl.  All rights reserved.
  *
