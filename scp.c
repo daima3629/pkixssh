b43ddf9ca9438b4622830311d32b7830d8004008
Roumen Petrov <pkixssh@roumenpetrov.info>
2019-02-01 19:56:47 +0200

    add "sanitize scp filenames via snmprintf"
    To do this we move the progressmeter formatting outside of signal handler context and have the atomicio callback called for EINTR too.
    Also force an update of progressmeter at the beginning and end of each transfer - allows very quick transfers to display the progressmeter.

--- scp.c
+++ scp.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: scp.c,v 1.199 2019/01/21 22:50:42 tb Exp $ */
+/* $OpenBSD: scp.c,v 1.201 2019/01/24 16:52:17 dtucker Exp $ */
 /*
  * scp - secure remote copy.  This is basically patched BSD rcp which
  * uses ssh to do the data transfer (instead of using rcmd).
@@ -586,6 +586,7 @@ scpio(void *_cnt, size_t s)
 	off_t *cnt = (off_t *)_cnt;
 
 	*cnt += s;
+	refresh_progress_meter(0);
 	if (limit_kbps > 0)
 		bandwidth_limit(&bwlimit, s);
 	return 0;
