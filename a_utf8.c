3364e792be2816e7108bb0a59e7502eb46fa7de6
Roumen Petrov <pkixssh@roumenpetrov.info>
2019-04-04 19:21:51 +0300

    synchronize a_utf8.c with current "Apache License 2.0"

--- a_utf8.c
+++ a_utf8.c
@@ -1,17 +1,12 @@
 /*
  * Copyright 1995-2016 The OpenSSL Project Authors. All Rights Reserved.
  *
- * Licensed under the OpenSSL license (the "License").  You may not use
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
  * in the file LICENSE in the source distribution or at
  * https://www.openssl.org/source/license.html
  */
 
-#if 0
-#include <stdio.h>
-#include "internal/cryptlib.h"
-#include <openssl/asn1.h>
-#endif
 #include "evp-compat.h"
 
 #ifdef HAVE_UTF8_GETC
