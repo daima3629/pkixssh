d43ca8d442f2fce6773d54656f0c61505dfed84a
Roumen Petrov <pkixssh@roumenpetrov.info>
2019-06-14 19:03:25 +0300

    rewrite user key authorization function
    Ensure that X.509 key is validated if is used authorized keys command and if validation is not first.
    Check authorization files first.
    Properly free authentication options (prevents possible memory leak in error path).
    Improve code readability.

--- auth2-pubkey.c
+++ auth2-pubkey.c
@@ -1144,44 +1144,41 @@ user_xkey_allowed(struct ssh *ssh, struct passwd *pw, ssh_sign_ctx *ctx,
 		*authoptsp = NULL;
 
 	if (options.x509flags.validate_first &&
-	    sshkey_is_x509(key)
-	) {
-		if (xkey_validate_cert(key) < 0) return 0;
-	}
-
+	    sshkey_is_x509(key) &&
+	    (xkey_validate_cert(key) < 0))
+		return 0;
 	if (auth_key_is_revoked(key))
 		return 0;
 	if (sshkey_is_cert(key) &&
 	    auth_key_is_revoked(key->cert->signature_key))
 		return 0;
 
-	if ((success = user_cert_trusted_ca(ssh, pw, key, &opts)) != 0)
-		goto out;
-	sshauthopt_free(opts);
-	opts = NULL;
-
-	if ((success = user_key_command_allowed2(ssh, pw, key, &opts)) != 0)
-		goto out;
-	sshauthopt_free(opts);
-	opts = NULL;
-
-	for (i = 0; !success && i < options.num_authkeys_files; i++) {
+	for (i = 0; i < options.num_authkeys_files; i++) {
 		if (strcasecmp(options.authorized_keys_files[i], "none") == 0)
 			continue;
 		file = expand_authorized_keys(
 		    options.authorized_keys_files[i], pw);
 		success = user_key_allowed2(ssh, pw, key, file, &opts);
 		free(file);
+		if (success) goto out;
+		sshauthopt_free(opts);
+		opts = NULL;
 	}
 
+	success = user_cert_trusted_ca(ssh, pw, key, &opts);
+	if (success) goto out;
+	sshauthopt_free(opts);
+	opts = NULL;
+
+	success = user_key_command_allowed2(ssh, pw, key, &opts);
+
+ out:
 	if (success &&
 	    !options.x509flags.validate_first &&
 	    sshkey_is_x509(key)
-	) {
-		if (xkey_validate_cert(key) < 0) return 0;
-	}
+	)
+		success = (xkey_validate_cert(key) == SSH_ERR_SUCCESS);
 
- out:
 	if (success && authoptsp != NULL) {
 		*authoptsp = opts;
 		opts = NULL;
