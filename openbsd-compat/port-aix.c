ed010104f4804659b4c13c7809b082e00b09973c
Roumen Petrov <pkixssh@roumenpetrov.info>
2019-11-13 19:06:03 +0200

    pick "put headers inside ifdef _AIX" AIX port
    Prevents compile errors due to missing definitions (eg va_list) on non-AIX platforms.

--- openbsd-compat/port-aix.c
+++ openbsd-compat/port-aix.c
@@ -26,6 +26,8 @@
  */
 #include "includes.h"
 
+#ifdef _AIX
+
 #include "xmalloc.h"
 #include "sshbuf.h"
 #include "ssherr.h"
@@ -34,8 +36,6 @@
 #include "ssh.h"
 #include "ssh_api.h"
 #include "log.h"
-
-#ifdef _AIX
 
 #include <errno.h>
 #if defined(HAVE_NETDB_H)
