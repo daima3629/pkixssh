f25226940d90b7525348a2fdfb890d752410e704
Roumen Petrov <pkixssh@roumenpetrov.info>
2020-06-19 19:15:34 +0300

    regression test that ssh-agent exits when running a command
    (adaptation)

---
+++ regress/agent-subprocess.sh
@@ -0,0 +1,23 @@
+#	$OpenBSD: agent-subprocess.sh,v 1.1 2020/06/19 05:07:09 dtucker Exp $
+#	Placed in the Public Domain.
+
+tid="agent subprocess"
+
+trace "ensure agent exits when run as subprocess"
+$SSHAGENT $TEST_SHELL -c "echo \$SSH_AGENT_PID >$OBJ/pidfile; sleep 1"
+
+pid=`cat $OBJ/pidfile`
+
+echo "waiting 10s agent to exit ..."
+# currently ssh-agent polls every 10s so we need to wait at least that long.
+count=12
+while test ${count} -gt 0 && kill -0 $pid >/dev/null 2>&1 ; do
+	count=`expr ${count} - 1`
+	sleep 1
+done
+
+if test ${count} -eq 0; then
+	fail "agent still running"
+fi
+
+rm -f $OBJ/pidfile
