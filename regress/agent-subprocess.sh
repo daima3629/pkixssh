d4f7085aee0f07d9eb8d78acfe28d173fb703697
Roumen Petrov <pkixssh@roumenpetrov.info>
2020-06-22 19:14:54 +0300

    rewrite "regression test that ssh-agent exits when running a command"
    Ensure correct processing if counter is one.

--- regress/agent-subprocess.sh
+++ regress/agent-subprocess.sh
@@ -5,19 +5,22 @@ tid="agent subprocess"
 
 trace "ensure agent exits when run as subprocess"
 $SSHAGENT $TEST_SHELL -c "echo \$SSH_AGENT_PID >$OBJ/pidfile; sleep 1"
-
+sleep 1
 pid=`cat $OBJ/pidfile`
 
-echo "waiting 10s agent to exit ..."
+echo "waiting about 10s agent to exit ..."
 # currently ssh-agent polls every 10s so we need to wait at least that long.
 count=12
-while test ${count} -gt 0 && kill -0 $pid >/dev/null 2>&1 ; do
-	count=`expr ${count} - 1`
-	sleep 1
+while test $count -gt 0 ; do
+  sleep 1
+  if ! kill -0 $pid >/dev/null 2>&1 ; then
+    break
+  fi
+  count=`expr $count - 1`
 done
 
-if test ${count} -eq 0; then
-	fail "agent still running"
+if test $count -eq 0 ; then
+  fail "agent still running"
 fi
 
 rm -f $OBJ/pidfile
