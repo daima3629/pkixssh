d7d25af9d6fa330126ff0bcce867ed17c3904771
Roumen Petrov <pkixssh@roumenpetrov.info>
2020-12-02 19:32:15 +0200

    switch more functions that still encode function name in format message to use new macros

--- sshkey.c
+++ sshkey.c
@@ -2495,7 +2495,7 @@ sshkey_from_blob_internal(struct sshbuf *b, struct sshkey **keyp,
 	}
 
 	type = sshkey_type_from_name(ktype);
-	debug3("key_from_blob(..., ...) ktype=%.30s", ktype);
+	debug3_f("ktype=%.30s", ktype);
 	if (!allow_cert && sshkey_type_is_cert(type)) {
 		ret = SSH_ERR_KEY_CERT_INVALID_SIGN_KEY;
 		goto out;
@@ -2600,7 +2600,7 @@ sshkey_from_blob_internal(struct sshbuf *b, struct sshkey **keyp,
 			goto out;
 		}
 		key->ecdsa_nid = sshkey_ecdsa_nid_from_name(ktype);
-		debug3("key_from_blob(..., ...) ktype/nid=%.50s / %d", ktype, key->ecdsa_nid);
+		debug3_f("ktype/nid=%.50s / %d", ktype, key->ecdsa_nid);
 		if (sshbuf_get_cstring(b, &curve, NULL) != 0) {
 			ret = SSH_ERR_INVALID_FORMAT;
 			goto out;
