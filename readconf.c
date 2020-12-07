6c8a5cd5b022e2caac66a18beb48819686c48e86
Roumen Petrov <pkixssh@roumenpetrov.info>
2020-12-07 19:34:28 +0200

    ignore comments at the end of config lines in client configuration similar to daemon configuration
    (adaptation)

--- readconf.c
+++ readconf.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: readconf.c,v 1.342 2020/11/15 22:34:58 djm Exp $ */
+/* $OpenBSD: readconf.c,v 1.343 2020/11/30 05:36:39 dtucker Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -2128,6 +2128,14 @@ read_config_file_depth(const char *filename, struct passwd *pw,
 	while (getline(&line, &linesize, f) != -1) {
 		/* Update line number counter. */
 		linenum++;
+		/*
+		 * Trim out comments and strip whitespace.
+		 * NB - preserve newlines, they are needed to reproduce
+		 * line numbers later for error messages.
+		 */
+	{	char *cp = strchr(line, '#');
+		if (cp != NULL) *cp = '\0';
+	}
 		if (process_config_line_depth(options, pw, host, original_host,
 		    line, filename, linenum, activep, flags, want_final_pass,
 		    depth) != 0)
