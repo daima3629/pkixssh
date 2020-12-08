922ff1e27d72dae7b7fe8956a30c284b4dc77855
Roumen Petrov <pkixssh@roumenpetrov.info>
2020-12-08 19:27:25 +0200

    return only error code if fail to decode file transfer attribute

--- sftp-common.c
+++ sftp-common.c
@@ -41,7 +41,6 @@
 #endif
 
 #include "xmalloc.h"
-#include "ssherr.h"
 #include "sshbuf.h"
 #include "log.h"
 #include "misc.h"
@@ -135,7 +134,7 @@ decode_attrib(struct sshbuf *b, Attrib *a)
 		u_int i, count;
 
 		if ((r = sshbuf_get_u32(b, &count)) != 0)
-			fatal("%s: buffer error: %s", __func__, ssh_err(r));
+			return r;
 		for (i = 0; i < count; i++) {
 			if ((r = sshbuf_get_cstring(b, &type, NULL)) != 0 ||
 			    (r = sshbuf_get_string(b, &data, &dlen)) != 0)
