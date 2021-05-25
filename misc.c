291bf11868f41ccbfe7764a1493e346fdcb2d5ec
Roumen Petrov <pkixssh@roumenpetrov.info>
2021-05-25 19:25:45 +0300

    return postive if O_NONBLOCK flag is changed
    Also modernize C return-style.

--- misc.c
+++ misc.c
@@ -106,20 +106,20 @@ set_nonblock(int fd)
 	val = fcntl(fd, F_GETFL);
 	if (val == -1) {
 		error("fcntl(%d, F_GETFL): %s", fd, strerror(errno));
-		return (-1);
+		return -1;
 	}
 	if (val & O_NONBLOCK) {
 		debug3("fd %d is O_NONBLOCK", fd);
-		return (0);
+		return 0;
 	}
 	debug2("fd %d setting O_NONBLOCK", fd);
 	val |= O_NONBLOCK;
 	if (fcntl(fd, F_SETFL, val) == -1) {
 		debug("fcntl(%d, F_SETFL, O_NONBLOCK): %s", fd,
 		    strerror(errno));
-		return (-1);
+		return -1;
 	}
-	return (0);
+	return 1;
 }
 
 int
@@ -130,20 +130,20 @@ unset_nonblock(int fd)
 	val = fcntl(fd, F_GETFL);
 	if (val == -1) {
 		error("fcntl(%d, F_GETFL): %s", fd, strerror(errno));
-		return (-1);
+		return -1;
 	}
 	if (!(val & O_NONBLOCK)) {
 		debug3("fd %d is not O_NONBLOCK", fd);
-		return (0);
+		return 0;
 	}
 	debug("fd %d clearing O_NONBLOCK", fd);
 	val &= ~O_NONBLOCK;
 	if (fcntl(fd, F_SETFL, val) == -1) {
 		debug("fcntl(%d, F_SETFL, ~O_NONBLOCK): %s",
 		    fd, strerror(errno));
-		return (-1);
+		return -1;
 	}
-	return (0);
+	return 1;
 }
 
 const char *
