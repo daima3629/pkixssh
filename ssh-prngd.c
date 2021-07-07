0e0b54ae1ad7ba3f398335976848312a6aafc822
Roumen Petrov <pkixssh@roumenpetrov.info>
2021-07-07 19:28:44 +0300

    move use of entropy daemon into own source
    Note mostly outdated code as PKIX-SSH requires OpenSSL and usually OpenSSL "internally seeded".
    Remark: OpenSSL 1.1.0 requires EGD daemon support to be enabled explicitly when configuring.
    
    (adaptation)

---
+++ ssh-prngd.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021 Roumen Petrov.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* NOTE:
+ * Move prngd to compatibily library is not correct as the source is part of
+ * ssh functionality. It is not compatibily source.
+ * Also move adds cyclic dependency to atomicio. To avoid this lets keep
+ * prngd source  as part of libssh and in same time to minimise source
+ * code differences.
+ */
+#include "openbsd-compat/port-prngd.c"
