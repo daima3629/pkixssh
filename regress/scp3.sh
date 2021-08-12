5401cc42ffec37f8031c66e25dd39bfe2ec9dbf1
Roumen Petrov <pkixssh@roumenpetrov.info>
2021-08-12 19:24:51 +0300

    regression test for scp -3
    (adaptation)

---
+++ regress/scp3.sh
@@ -0,0 +1,52 @@
+#	$OpenBSD: scp3.sh,v 1.2 2021/08/06 09:00:18 dtucker Exp $
+#	Placed in the Public Domain.
+
+tid="scp3"
+
+#set -x
+
+COPY2=${OBJ}/copy2
+DIR=${COPY}.dd
+DIR2=${COPY}.dd2
+
+SRC=`dirname ${SCRIPT}`
+
+scpclean() {
+	rm -rf ${COPY} ${COPY2} ${DIR} ${DIR2}
+	mkdir ${DIR} ${DIR2}
+	chmod 755 ${DIR} ${DIR2}
+}
+
+# XXX sftp too once it's ready
+for mode in scp ; do
+	scpopts="-F${OBJ}/ssh_proxy -S ${SSH} -q"
+	tag="$tid: $mode mode"
+
+	verbose "$tag: simple copy remote file to remote file"
+	scpclean
+	$SCP $scpopts -3 hostA:${DATA} hostB:${COPY} || fail "copy failed"
+	cmp ${DATA} ${COPY} || fail "corrupted copy"
+
+	verbose "$tag: simple copy remote file to remote dir"
+	scpclean
+	cp ${DATA} ${COPY}
+	$SCP $scpopts -3 hostA:${COPY} hostB:${DIR} || fail "copy failed"
+	cmp ${COPY} ${DIR}/copy || fail "corrupted copy"
+
+	verbose "$tag: recursive remote dir to remote dir"
+	scpclean
+	rm -rf ${DIR2}
+	cp ${DATA} ${DIR}/copy
+	$SCP $scpopts -3r hostA:${DIR} hostB:${DIR2} || fail "copy failed"
+	diff -r ${DIR} ${DIR2} || fail "corrupted copy"
+	diff -r ${DIR2} ${DIR} || fail "corrupted copy"
+
+	verbose "$tag: detect non-directory target"
+	scpclean
+	echo a > ${COPY}
+	echo b > ${COPY2}
+	$SCP $scpopts -3 hostA:${DATA} hostA:${COPY} hostB:${COPY2}
+	cmp ${COPY} ${COPY2} >/dev/null && fail "corrupt target"
+done
+
+scpclean
