40739de7336fd0d332942c43c295ff244fe18013
Roumen Petrov <pkixssh@roumenpetrov.info>
2021-10-09 19:10:37 +0300

    update hostkey-agent regression test to generate keys and custom certificates
    Reserved for future use.

--- regress/hostkey-agent.sh
+++ regress/hostkey-agent.sh
@@ -1,10 +1,10 @@
-#	$OpenBSD: hostkey-agent.sh,v 1.11 2019/12/16 02:39:05 djm Exp $
+#	$OpenBSD: hostkey-agent.sh,v 1.12 2021/09/29 01:32:21 djm Exp $
 #	Placed in the Public Domain.
 
 tid="hostkey agent"
 PLAIN_TYPES=`echo "$SSH_HOSTKEY_TYPES" | sed 's/ssh-xmss@openssh.com//'` # TODO
 
-rm -f $OBJ/agent-key.* $OBJ/ssh_proxy.orig $OBJ/known_hosts.orig
+rm -f $OBJ/agent-key.* $OBJ/ssh_proxy.orig $OBJ/agent-ca
 
 trace "start agent"
 eval `${SSHAGENT} ${EXTRA_AGENT_ARGS} -s` > /dev/null
@@ -14,39 +14,43 @@ r=$?
 grep -vi 'hostkey' $OBJ/sshd_proxy > $OBJ/sshd_proxy.orig
 echo "HostKeyAgent $SSH_AUTH_SOCK" >> $OBJ/sshd_proxy.orig
 
+trace "make CA key"
+
+${SSHKEYGEN} -qt ed25519 -f $OBJ/agent-ca -N '' || fatal "ssh-keygen CA"
+
 trace "load hostkeys"
 for k in $PLAIN_TYPES ; do
 	${SSHKEYGEN} -qt $k -f $OBJ/agent-key.$k -N '' || fatal "ssh-keygen $k"
-	(
-		printf 'localhost-with-alias,127.0.0.1,::1 '
-		cat $OBJ/agent-key.$k.pub
-	) >> $OBJ/known_hosts.orig
-	${SSHADD} $OBJ/agent-key.$k >/dev/null 2>&1 || \
+	${SSHKEYGEN} -s $OBJ/agent-ca -qh -n localhost-with-alias \
+		-I localhost-with-alias $OBJ/agent-key.$k.pub || \
+		fatal "sign $k"
+	${SSHADD} -k $OBJ/agent-key.$k >/dev/null 2>&1 || \
 		fatal "couldn't load key $OBJ/agent-key.$k"
-	echo "Hostkey $OBJ/agent-key.${k}" >> $OBJ/sshd_proxy.orig
 	# Remove private key so the server can't use it.
 	rm $OBJ/agent-key.$k || fatal "couldn't rm $OBJ/agent-key.$k"
 done
-cp $OBJ/known_hosts.orig $OBJ/known_hosts
+rm $OBJ/agent-ca # Don't need CA private any more either
 
 unset SSH_AUTH_SOCK
 
 for ps in $SSHD_PRIVSEP ; do
-	for k in $PLAIN_TYPES ; do
-		verbose "key type $k privsep=$ps"
-		cp $OBJ/sshd_proxy.orig $OBJ/sshd_proxy
-		echo "UsePrivilegeSeparation $ps" >> $OBJ/sshd_proxy
-		echo "HostKeyAlgorithms $k" >> $OBJ/sshd_proxy
-		opts="-oHostKeyAlgorithms=$k -F $OBJ/ssh_proxy"
-		cp $OBJ/known_hosts.orig $OBJ/known_hosts
-		SSH_CONNECTION=`${SSH} $opts host 'echo $SSH_CONNECTION'`
-		if [ $? -ne 0 ]; then
-			fail "privsep=$ps failed"
-		fi
-		if [ "$SSH_CONNECTION" != "UNKNOWN 65535 UNKNOWN 65535" ]; then
-			fail "bad SSH_CONNECTION key type $k privsep=$ps"
-		fi
-	done
+for k in $PLAIN_TYPES ; do
+	verbose "key type $k privsep=$ps"
+	cp $OBJ/sshd_proxy.orig $OBJ/sshd_proxy
+	echo "UsePrivilegeSeparation $ps" >> $OBJ/sshd_proxy
+	echo "HostKeyAlgorithms $k" >> $OBJ/sshd_proxy
+	echo "Hostkey $OBJ/agent-key.${k}" >> $OBJ/sshd_proxy
+	opts="-oHostKeyAlgorithms=$k -F $OBJ/ssh_proxy"
+	( printf 'localhost-with-alias,127.0.0.1,::1 ' ;
+	  cat $OBJ/agent-key.$k.pub) > $OBJ/known_hosts
+	SSH_CONNECTION=`${SSH} $opts host 'echo $SSH_CONNECTION'`
+	if [ $? -ne 0 ]; then
+		fail "keytype $k privsep=$ps failed"
+	fi
+	if [ "$SSH_CONNECTION" != "UNKNOWN 65535 UNKNOWN 65535" ]; then
+		fail "bad SSH_CONNECTION key type $k privsep=$ps"
+	fi
+done
 done
 
 trace "kill agent"
