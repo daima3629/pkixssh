301d7714981cd617eafe5c9eeb2b0d6ad8062caf
Roumen Petrov <pkixssh@roumenpetrov.info>
2022-01-07 19:26:13 +0200

    correct some spelling errors
    (synchronisation)

--- sshbuf.h
+++ sshbuf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sshbuf.h,v 1.22 2020/06/05 03:24:36 djm Exp $	*/
+/*	$OpenBSD: sshbuf.h,v 1.24 2022/01/01 05:55:06 jsg Exp $	*/
 /*
  * Copyright (c) 2011 Damien Miller
  * Copyright (c) 2020 Roumen Petrov
@@ -380,7 +380,7 @@ u_int	sshbuf_refcount(const struct sshbuf *buf);
 
 # define SSHBUF_SIZE_INIT	256		/* Initial allocation */
 # define SSHBUF_SIZE_INC	256		/* Preferred increment length */
-# define SSHBUF_PACK_MIN	8192		/* Minimim packable offset */
+# define SSHBUF_PACK_MIN	8192		/* Minimum packable offset */
 
 /* # define SSHBUF_ABORT abort */
 /* # define SSHBUF_DEBUG */
