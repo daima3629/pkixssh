5c5f915e7a3e52a4741cb52f941160fd843bfcbf
Roumen Petrov <pkixssh@roumenpetrov.info>
2022-02-14 19:30:18 +0200

    simplify algorithm logic for rsa keys
    Use only primitives from cryptografic library.

--- ssh-rsa.c
+++ ssh-rsa.c
@@ -2,7 +2,7 @@
 /*
  * Copyright (c) 2000, 2003 Markus Friedl <markus@openbsd.org>
  * Copyright (c) 2011 Dr. Stephen Henson.  All rights reserved.
- * Copyright (c) 2011-2021 Roumen Petrov.  All rights reserved.
+ * Copyright (c) 2011-2022 Roumen Petrov.  All rights reserved.
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -35,78 +35,45 @@
 #include "ssherr.h"
 #define SSHKEY_INTERNAL
 #include "sshkey.h"
-#include "digest.h"
 #include "xmalloc.h"
 #include "log.h"
 
-static const char *
-rsa_hash_alg_ident(int hash_alg)
-{
-	switch (hash_alg) {
-	case SSH_DIGEST_SHA1:
-		return "ssh-rsa";
+
+struct ssh_rsa_alg_st {
+	const char *name;
+	const int nid;
+	const char *signame;
+};
+
+static struct ssh_rsa_alg_st
+ssh_rsa_algs[] = {
 #ifdef HAVE_EVP_SHA256
-	case SSH_DIGEST_SHA256:
-		return "rsa-sha2-256";
-	case SSH_DIGEST_SHA512:
-		return "rsa-sha2-512";
-#endif /*def HAVE_EVP_SHA256*/
-	}
+	{ "rsa-sha2-256", NID_sha256, "rsa-sha2-256" },
+	{ "rsa-sha2-512", NID_sha512, "rsa-sha2-512" },
+#endif
+	{ "ssh-rsa", NID_sha1, "ssh-rsa" },
+#ifdef HAVE_EVP_SHA256
+	{ "rsa-sha2-256-cert-v01@openssh.com", NID_sha256, "rsa-sha2-256" },
+	{ "rsa-sha2-512-cert-v01@openssh.com", NID_sha512, "rsa-sha2-512" },
+#endif
+	{ "ssh-rsa-cert-v01@openssh.com", NID_sha1, "ssh-rsa" },
+	{ NULL, NID_undef, NULL }
+};
+
+static struct ssh_rsa_alg_st* ssh_rsa_alg_info(const char *alg);
+
+struct ssh_rsa_alg_st*
+ssh_rsa_alg_info(const char *alg) {
+	struct ssh_rsa_alg_st* p;
+
+	if (alg == NULL || *alg == '\0')
+		return ssh_rsa_alg_info("ssh-rsa");
+
+	for (p = ssh_rsa_algs; p->name != NULL; p++)
+		if (strcmp(alg, p->name) == 0)
+			return p;
+
 	return NULL;
-}
-
-/*
- * Returns the hash algorithm ID for a given algorithm identifier as used
- * inside the signature blob,
- */
-static int
-rsa_hash_id_from_ident(const char *ident)
-{
-	if (strcmp(ident, "ssh-rsa") == 0)
-		return SSH_DIGEST_SHA1;
-#ifdef HAVE_EVP_SHA256
-	if (strcmp(ident, "rsa-sha2-256") == 0)
-		return SSH_DIGEST_SHA256;
-	if (strcmp(ident, "rsa-sha2-512") == 0)
-		return SSH_DIGEST_SHA512;
-#endif /*def HAVE_EVP_SHA256*/
-	return -1;
-}
-
-/*
- * Return the hash algorithm ID for the specified key name. This includes
- * all the cases of rsa_hash_id_from_ident() but also the certificate key
- * types.
- */
-static int
-rsa_hash_id_from_keyname(const char *alg)
-{
-	int r;
-
-	if ((r = rsa_hash_id_from_ident(alg)) != -1)
-		return r;
-	if (strcmp(alg, "ssh-rsa-cert-v01@openssh.com") == 0)
-		return SSH_DIGEST_SHA1;
-	if (strcmp(alg, "rsa-sha2-256-cert-v01@openssh.com") == 0)
-		return SSH_DIGEST_SHA256;
-	if (strcmp(alg, "rsa-sha2-512-cert-v01@openssh.com") == 0)
-		return SSH_DIGEST_SHA512;
-	return -1;
-}
-
-static int
-rsa_hash_alg_nid(int type)
-{
-	switch (type) {
-	case SSH_DIGEST_SHA1:
-		return NID_sha1;
-	case SSH_DIGEST_SHA256:
-		return NID_sha256;
-	case SSH_DIGEST_SHA512:
-		return NID_sha512;
-	default:
-		return -1;
-	}
 }
 
 int
@@ -123,7 +90,8 @@ ssh_rsa_sign(const struct sshkey *key, u_char **sigp, size_t *lenp,
 	u_char *sig = NULL;
 	size_t slen = 0;
 	u_int len;
-	int nid, hash_alg, ret;
+	struct ssh_rsa_alg_st *alg_info;
+	int ret;
 	struct sshbuf *b = NULL;
 
 	if (lenp != NULL)
@@ -131,30 +99,20 @@ ssh_rsa_sign(const struct sshkey *key, u_char **sigp, size_t *lenp,
 	if (sigp != NULL)
 		*sigp = NULL;
 
-	if (alg_ident == NULL || strlen(alg_ident) == 0)
-		hash_alg = SSH_DIGEST_SHA1;
-	else
-		hash_alg = rsa_hash_id_from_keyname(alg_ident);
-{	const char *s = rsa_hash_alg_ident(hash_alg);
-	debug3_f("hash_alg=%d/%s", hash_alg, s != NULL ? s: "");
-}
-	if (hash_alg == -1)
+	alg_info = ssh_rsa_alg_info(alg_ident);
+	if (alg_info == NULL)
 		return SSH_ERR_INVALID_ARGUMENT;
+	debug3_f("alg=%s/%s", (alg_ident != NULL ? alg_ident : "(nil)"), alg_info->name);
 
 	ret = sshkey_validate_public_rsa(key);
 	if (ret != 0) return ret;
-
-	/* hash the data */
-	nid = rsa_hash_alg_nid(hash_alg);
 
 {	/* EVP_Sign... */
 	const EVP_MD *evp_md;
 	int ok = -1;
 
-	sig = NULL;
-
-	if ((evp_md = EVP_get_digestbynid(nid)) == NULL) {
-		error_f("EVP_get_digestbynid %d failed", nid);
+	if ((evp_md = EVP_get_digestbynid(alg_info->nid)) == NULL) {
+		error_f("EVP_get_digestbynid %d failed", alg_info->nid);
 		ret = SSH_ERR_INTERNAL_ERROR;
 		goto out;
 	}
@@ -219,7 +177,7 @@ evp_end:
 		ret = SSH_ERR_ALLOC_FAIL;
 		goto out;
 	}
-	if ((ret = sshbuf_put_cstring(b, rsa_hash_alg_ident(hash_alg))) != 0 ||
+	if ((ret = sshbuf_put_cstring(b, alg_info->signame)) != 0 ||
 	    (ret = sshbuf_put_string(b, sig, slen)) != 0)
 		goto out;
 	len = sshbuf_len(b);
@@ -245,7 +203,8 @@ ssh_rsa_verify(const struct sshkey *key,
     const char *alg)
 {
 	char *sigtype = NULL;
-	int hash_alg, ret;
+	struct ssh_rsa_alg_st *alg_info;
+	int ret;
 	size_t len = 0, diff, modlen;
 	struct sshbuf *b = NULL;
 	u_char *osigblob, *sigblob = NULL;
@@ -262,22 +221,25 @@ ssh_rsa_verify(const struct sshkey *key,
 		ret = SSH_ERR_INVALID_FORMAT;
 		goto out;
 	}
-	if ((hash_alg = rsa_hash_id_from_ident(sigtype)) == -1) {
-		ret = SSH_ERR_KEY_TYPE_MISMATCH;
+	/* algorithm for plain keys */
+	alg_info = ssh_rsa_alg_info(sigtype);
+	if (alg_info == NULL) {
+		ret = SSH_ERR_INVALID_ARGUMENT;
 		goto out;
 	}
-	debug3_f("hash_alg=%d/%s", hash_alg, rsa_hash_alg_ident(hash_alg));
+	debug3_f("alg=%s/%s", (alg != NULL ? alg : "(nil)"), alg_info->name);
 	/*
 	 * For legacy reasons allow ssh-rsa-cert-v01 certs to accept SHA2 signatures
 	 * but otherwise the signature algorithm should match.
 	 */
 	if (alg != NULL && strcmp(alg, "ssh-rsa-cert-v01@openssh.com") != 0) {
-		int want_alg;
-		if ((want_alg = rsa_hash_id_from_keyname(alg)) == -1) {
+		struct ssh_rsa_alg_st *want_info;
+		want_info = ssh_rsa_alg_info(alg);
+		if (want_info == NULL) {
 			ret = SSH_ERR_INVALID_ARGUMENT;
 			goto out;
 		}
-		if (hash_alg != want_alg) {
+		if (alg_info->nid != want_info->nid) {
 			ret = SSH_ERR_SIGNATURE_INVALID;
 			goto out;
 		}
@@ -309,13 +271,11 @@ ssh_rsa_verify(const struct sshkey *key,
 	}
 
 {	/* EVP_Verify... */
-	int nid;
 	int ok;
 	const EVP_MD *evp_md;
 
-	nid = rsa_hash_alg_nid(hash_alg);
-	if ((evp_md = EVP_get_digestbynid(nid)) == NULL) {
-		error_f("EVP_get_digestbynid %d failed", nid);
+	if ((evp_md = EVP_get_digestbynid(alg_info->nid)) == NULL) {
+		error_f("EVP_get_digestbynid %d failed", alg_info->nid);
 		ret = SSH_ERR_INTERNAL_ERROR;
 		goto out;
 	}
