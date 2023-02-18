2c2bae8573ba3e3e674d42c82a993a2f86782e2e
Roumen Petrov <pkixssh@roumenpetrov.info>
2023-02-18 08:30:43 +0200

    remove death secsh "compatibility" flag SSH_BUG_BIGENDIANAES+
    Fix previous.

--- compat.c
+++ compat.c
@@ -140,13 +140,7 @@ compat_datafellows(const char *version)
 char *
 compat_cipher_proposal(struct ssh *ssh, char *cipher_prop)
 {
-	debug2_f("original cipher proposal: %s", cipher_prop);
-	if ((cipher_prop = match_filter_denylist(cipher_prop, "aes*")) == NULL)
-		fatal("match_filter_denylist failed");
-	debug2_f("compat cipher proposal: %s", cipher_prop);
-	if (*cipher_prop == '\0')
-		fatal("No supported ciphers found");
-	return cipher_prop;
+	return xstrdup(cipher_prop);
 }
 
 /* Always returns pointer to allocated memory, caller must free. */
