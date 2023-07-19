2e79b8f5db0610c59818bb00fbad91f6dfaa8fab
Roumen Petrov <pkixssh@roumenpetrov.info>
2023-07-19 19:44:55 +0300

    return "bad magic" error when is parsed beginning of KRL
    (synchronisation)

--- krl.c
+++ krl.c
@@ -1,3 +1,4 @@
+/* $OpenBSD: krl.c,v 1.59 2023/07/17 05:22:30 djm Exp $ */
 /*
  * Copyright (c) 2012 Damien Miller <djm@mindrot.org>
  *
@@ -13,8 +14,6 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-
-/* $OpenBSD: krl.c,v 1.57 2023/07/17 04:01:10 djm Exp $ */
 
 #include "includes.h"
 
@@ -979,7 +978,7 @@ ssh_krl_from_blob(struct sshbuf *buf, struct ssh_krl **krlp)
 	/* KRL must begin with magic string */
 	if ((r = sshbuf_cmp(buf, 0, KRL_MAGIC, sizeof(KRL_MAGIC) - 1)) != 0) {
 		debug2_f("bad KRL magic header");
-		return r;
+		return SSH_ERR_KRL_BAD_MAGIC;
 	}
 
 	if ((krl = ssh_krl_init()) == NULL) {
