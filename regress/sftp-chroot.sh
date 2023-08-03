8007ff7a12595c82da3d3bb5f2b205b2aad45f67
Roumen Petrov <pkixssh@roumenpetrov.info>
2023-08-03 19:35:53 +0300

    regression test for ChrootDirectory in Match block
    (adaptation)

--- regress/sftp-chroot.sh
+++ regress/sftp-chroot.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: sftp-chroot.sh,v 1.8 2021/09/01 00:50:27 dtucker Exp $
+#	$OpenBSD: sftp-chroot.sh,v 1.9 2023/05/12 06:37:42 djm Exp $
 #	Placed in the Public Domain.
 
 tid="sftp in chroot"
@@ -16,13 +16,31 @@ if ! $OBJ/check-perm -m chroot "$CHROOT" ; then
   skip "$CHROOT is unsuitable as ChrootDirectory"
 fi
 
+verbose "test $tid: get"
 $SUDO sh -c "echo mekmitastdigoat > $PRIVDATA" || \
 	fatal "create $PRIVDATA failed"
 
-start_sshd -oChrootDirectory=$CHROOT -oForceCommand="internal-sftp -d /"
+echo "ForceCommand internal-sftp -d /" >> $OBJ/sshd_config
 
-verbose "test $tid: get"
+start_sshd -oChrootDirectory=$CHROOT
 ${SFTP} -S "$SSH" -F $OBJ/ssh_config host:/${FILENAME} $COPY \
     >>$TEST_REGRESS_LOGFILE 2>&1 || \
 	fatal "Fetch ${FILENAME} failed"
 cmp $PRIVDATA $COPY || fail "$PRIVDATA $COPY differ"
+stop_sshd
+
+verbose "test $tid: match"
+$SUDO sh -c "echo orpheanbeholder > $PRIVDATA" || \
+	fatal "create $PRIVDATA failed"
+
+cat << EOF >> $OBJ/sshd_config
+Match All
+	ChrootDirectory $CHROOT
+EOF
+
+start_sshd
+${SFTP} -S "$SSH" -F $OBJ/ssh_config host:/${FILENAME} $COPY \
+    >>$TEST_REGRESS_LOGFILE 2>&1 || \
+	fatal "Fetch ${FILENAME} failed"
+cmp $PRIVDATA $COPY || fail "$PRIVDATA $COPY differ"
+stop_sshd
