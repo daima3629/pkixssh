569f681573ce7951fc9fe77ba951581d5e22c693
Roumen Petrov <pkixssh@roumenpetrov.info>
2023-11-10 19:46:04 +0200

    refactor creation of "authorized keys" file in forcecommand regression test

--- regress/forcecommand.sh
+++ regress/forcecommand.sh
@@ -5,27 +5,28 @@ tid="forced command"
 
 cp $OBJ/sshd_proxy $OBJ/sshd_proxy_bak
 
-cp /dev/null $OBJ/authorized_keys_$USER
-for t in ${SSH_KEYTYPES}; do
-	printf 'command="true" ' >>$OBJ/authorized_keys_$USER
-	cat $OBJ/$t.pub >> $OBJ/authorized_keys_$USER
-done
+authorized_keys() {
+	cmd=$1
+	cp /dev/null $OBJ/authorized_keys_$USER
+	for t in ${SSH_KEYTYPES}; do
+		test -z "$cmd" || \
+			printf "command=\"$cmd\" " >>$OBJ/authorized_keys_$USER
+		cat $OBJ/$t.pub >> $OBJ/authorized_keys_$USER
+	done
+}
 
 trace "forced command in key option"
+authorized_keys true
 ${SSH} -F $OBJ/ssh_proxy somehost false || fail "forced command in key option"
-
-cp /dev/null $OBJ/authorized_keys_$USER
-for t in ${SSH_KEYTYPES}; do
-	printf 'command="false" ' >> $OBJ/authorized_keys_$USER
-	cat $OBJ/$t.pub >> $OBJ/authorized_keys_$USER
-done
 
 cp $OBJ/sshd_proxy_bak $OBJ/sshd_proxy
 echo "ForceCommand true" >> $OBJ/sshd_proxy
 
 trace "forced command in sshd_config overrides key option"
+authorized_keys false
 ${SSH} -F $OBJ/ssh_proxy somehost false || fail "forced command config"
 
+authorized_keys
 cp $OBJ/sshd_proxy_bak $OBJ/sshd_proxy
 echo "ForceCommand false" >> $OBJ/sshd_proxy
 echo "Match User $USER" >> $OBJ/sshd_proxy
