d0816f2b02a042f1600eb6cb11ce5bdde771da82
Roumen Petrov <pkixssh@roumenpetrov.info>
2023-12-19 19:27:03 +0200

    PKIX-SSH 14.4

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
-/* $OpenBSD: version.h,v 1.99 2023/10/04 04:04:09 djm Exp $ */
+/* $OpenBSD: version.h,v 1.100 2023/12/18 14:48:44 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_9.5"
+#define SSH_VERSION	"OpenSSH_9.6"
 
 #define SSH_RELEASE	PACKAGE_STRING ", " SSH_VERSION "p1"
