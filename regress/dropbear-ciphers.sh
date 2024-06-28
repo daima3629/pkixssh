78d21aff76e80f498d2f9dab84a30663ffd2eb11
Roumen Petrov <pkixssh@roumenpetrov.info>
2024-06-28 19:37:16 +0300

    work-arround for ciphers and macs detection in dropbear regression test
    Dropbear (2024.84,2024.85) regression: command options -c/-m help require host argument.
    
    (inspired)

--- regress/dropbear-ciphers.sh
+++ regress/dropbear-ciphers.sh
@@ -1,12 +1,15 @@
-#	$OpenBSD: dropbear-ciphers.sh,v 1.1 2023/10/20 06:56:45 dtucker Exp $
+#	$OpenBSD: dropbear-ciphers.sh,v 1.3 2024/06/20 08:23:18 dtucker Exp $
 #	Placed in the Public Domain.
 
 tid="dropbear ciphers"
 
 $REGRESS_INTEROP_DROPBEAR || { RESULT=1; skip "dropbear interop tests are not enabled"; }
 
-ciphers=`$DBCLIENT -c help 2>&1 | awk '/ ciphers: /{print $4}' | tr ',' ' '`
-macs=`$DBCLIENT -m help 2>&1 | awk '/ MACs: /{print $4}' | tr ',' ' '`
+ciphers=`$DBCLIENT -c help somehost 2>&1 | awk '/ ciphers: /{print $4}' | tr ',' ' '`
+macs=`$DBCLIENT -m help somehost 2>&1 | awk '/ MACs: /{print $4}' | tr ',' ' '`
+if test -z "$macs" -o -z "$ciphers" ; then
+	skip "dbclient query ciphers or macs failed"
+fi
 
 rm -f $OBJ/.dropbear/dbclient.log
 for c in $ciphers ; do
