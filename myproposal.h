075512cd17bc708aea881ed02616969b9fc724ca
Roumen Petrov <pkixssh@roumenpetrov.info>
2025-03-07 18:54:44 +0200

    arrange key exchange methods

--- myproposal.h
+++ myproposal.h
@@ -47,20 +47,20 @@
 #endif
 
 #define KEX_SERVER_KEX \
+	"curve448-sha512," \
 	"curve25519-sha256," \
 	"curve25519-sha256@libssh.org," \
-	"curve448-sha512," \
 	"ecdh-sha2-nistp256," \
 	"ecdh-sha2-nistp384," \
 	"ecdh-sha2-nistp521," \
 	"diffie-hellman-group-exchange-sha256," \
-	"diffie-hellman-group16-sha512," \
 	"diffie-hellman-group18-sha512," \
+	"diffie-hellman-group16-sha512," \
 	"diffie-hellman-group14-sha256," \
-	KEX_SNTRUP761X25519 \
-	KEX_MLKEM768X25519 \
-	"diffie-hellman-group15-sha512," \
 	"diffie-hellman-group17-sha512," \
+	"diffie-hellman-group15-sha512," \
+	KEX_MLKEM768X25519 \
+	KEX_SNTRUP761X25519 \
 	"diffie-hellman-group14-sha1"
 
 #define KEX_CLIENT_KEX KEX_SERVER_KEX
